ed1036e9ca4e864a8c1f4246d91f7bf4a2060d28
lebrozaurjagger <lebrozaur@icloud.com>
2023-11-19 20:29:43 +0300

    Initial Commit

---
+++ DeliVery Demo/DeliVery_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliVery_DemoApp.swift
+//  DeliVery Demo
+//
+//  Created by Phillip Jagger on 19.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeliVery_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
